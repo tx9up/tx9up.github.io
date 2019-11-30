f7d4605a7b7f1b154f63bd9d2d279e0a8d16856e
Thomas <ha8721@gmail.com>
2019-11-30 18:08:40 +0800

    Add more stuff

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -88,8 +88,6 @@
             dateStr += dateChi;
         }
 
-        console.log(values);
-
         dateStr += "在" + values.incident_location + "發生的" + values.incident_type + "行為，特衰政府" + values.condemn_lvl + "。";
         paragraphs.push(dateStr);
         paragraphs.push("");
@@ -107,9 +105,18 @@
         description += "行為，";
 
         if (typeof values.incident_consequences === "string") {
+            if (values.incident_consequences == 'others') {
+                values.incident_consequences = values.incident_consequences_others.trim();
+            }
+
             description += values.incident_consequences + "，行為";
         }
         else if (values.incident_consequences) {
+            if (values.incident_consequences.indexOf('others') !== -1) {
+                let index = values.incident_consequences.indexOf('others');
+                values.incident_consequences[index] = values.incident_consequences_others.trim();
+            }
+
             description += "不但會" + values.incident_consequences[0] + "，" + "更會";
             description += values.incident_consequences.slice(1).join("，") + "，行為";
         }
@@ -122,6 +129,22 @@
         end += "　　香巷作為法治社會完全不能容忍任何" + values.incident_type + "行為，特衰政府" + values.condemn_lvl + "，並會嚴正追究。";
         paragraphs.push(end);
         paragraphs.push("");
+
+        if (values.dlc) {
+            let dlc = [];
+
+            if (typeof values.dlc == "string") {
+                dlc.push(values.dlc);
+            }
+            else {
+                dlc = values.dlc;
+            }
+
+            if (dlc.indexOf('no_interference' !== -1)) {
+                paragraphs.push("　　政府重申，《基本法》確保香巷特區在「一國兩制」的方針下，依法實行「巷人治巷」和高度自治。香巷特區政府亦一直嚴格按照《基本法》、《香巷人權法案條例》及其他法例保障香巷市民的權利及自由。香巷的法治精神、執法機關的嚴謹執法及獨立的司法制度，一向得到國際社會的高度評價和認同。特區政府對外國議會在沒有證據證明內地機構曾跨境執法的情況下，動輒提出旨在干預香巷特區事務的法案草案，表示不能接受並極度遺憾。");
+                paragraphs.push("");
+            }
+        }
 
         paragraphs.push("完");
         paragraphs.push("");
@@ -138,6 +161,33 @@
         let form = $('form');
 
         // resetForm(form);
+
+        form.bind('change', (e) => {
+            let others = false;
+
+            form.serializeArray().forEach((el) => {
+                if (el.name == 'incident_consequences' && el.value == 'others') {
+                    others = true;
+                }
+            });
+
+            if (others) {
+                form.find('input[name="incident_consequences_others"]').attr('required', 'required').show();
+            }
+            else {
+                form.find('input[name="incident_consequences_others"]').removeAttr('required').hide();
+            }
+        });
+
+        form.find('input[name="incident_consequences"][value="others"]').bind('change', (e) => {
+            // console.log($(this).prop("checked"));
+            // if ($(this).val()) {
+            //     form.find('input[name="incident_consequences_others"]').attr('required', 'required').show();
+            // }
+            // else {
+            //     form.find('input[name="incident_consequences_others"]').removeAttr('required').hide();
+            // }
+        });
 
         form.bind('submit', (e) => {
             e.preventDefault();
